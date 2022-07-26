7f44d0729cd37ab7b51ffca72612407f8dfe4546
kimminju99 <mindori1019@gmail.com>
2022-07-26 21:50:06 +0900

    [2022.07.26] 숭실대에 marker 생성

--- app/src/main/java/com/kimchuu/map_practice/MainActivity.kt
+++ app/src/main/java/com/kimchuu/map_practice/MainActivity.kt
@@ -2,10 +2,38 @@ package com.kimchuu.map_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kimchuu.map_practice.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        lateinit var naverMap: NaverMap
+    }
+
+    private lateinit var mapFragment: MapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 지도 객체 생성
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
+
+        // 비동기로 onMapReady 호출
+        mapFragment = mapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        var marker = Marker(LatLng(37.4963462,126.9568865))
+        marker.map = naverMap
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 }
